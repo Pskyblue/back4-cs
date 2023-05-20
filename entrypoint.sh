c66101a876e1f17dd145beacedd6b0a704c16b2a
Pskyblue <104996155+Pskyblue@users.noreply.github.com>
2023-05-20 03:37:55 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'c664751b-b662-48f9-b2aa-2f0d4d337a7f'}
+UUID=${UUID:-'1a1da73a-db33-40e6-b932-cf7d98df19f3'}
 
 # 哪吒三个参数，不需要的话可以留空，删除或在这三行最前面加 # 以注释
 NEZHA_SERVER= 
@@ -10,8 +10,8 @@ NEZHA_PORT=
 NEZHA_KEY=
 
 # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-ARGO_AUTH=''
-ARGO_DOMAIN=
+ARGO_AUTH='{"AccountTag":"3773bdd3aa939a189b07265a7319c2f8","TunnelSecret":"nYMrkJ+CU9HaOdzNWTJHDP4Kr9udrPgFKxty4YLjPc4=","TunnelID":"ccbd2c78-7aa2-4dfe-b3de-30025e208627"}'
+ARGO_DOMAIN=back4cs.ailsidersaf.ga
 
 generate_config() {
   cat > config.json << EOF
